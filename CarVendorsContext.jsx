0d3f6a6fc2ee05ce95de4e856a37e876e2ef40c7
kalat <kalatur36438@gmail.com>
2024-10-18 23:43:52 +0300

    123

---
+++ CarVendorsContext.jsx
@@ -0,0 +1,17 @@
+﻿import React, { createContext, useContext, useState } from 'react';
+
+const CarVendorsContext = createContext();
+
+export const CarVendorsProvider = ({ children }) => {
+    const [vendors, setVendors] = useState([
+        { id: 1, name: 'Toyota' },
+        { id: 2, name: 'BMW' },
+        { id: 3, name: 'Mercedes' },
+    ]);
+
+    return (
+        <CarVendorsContext.Provider value={vendors}>
+            {children}
+        </CarVendorsContext.Provider>
+    );
+};
